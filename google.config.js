fc511283e9ad72107d1ba678d501569c7baff4c2
officeofjane <officeofjane@gmail.com>
2026-04-24 11:56:24 +0200

    set up gdocs

--- google.config.js
+++ google.config.js
@@ -1,11 +1,11 @@
 export default [
 	{
-		"id": "13N105hN1xlkz3xDUV3jXK9ljHXw946oGoO6DHFP_I6A",
+		"id": "1CxKqLCA0TvfIzSoJFDPo9XDFojJTpLPBUvypsnTuwZI",
 		"filepath": "src/data/copy.json"
 	},
-	{
-		"id": "1qT0GdGcYV3Z8nh7AVRNVt8B4BOjyYB5C9sbSG7nBOig",
-		"gid": "0",
-		"filepath": "src/data/test.csv"
-	}
+	// {
+	// 	"id": "1qT0GdGcYV3Z8nh7AVRNVt8B4BOjyYB5C9sbSG7nBOig",
+	// 	"gid": "0",
+	// 	"filepath": "src/data/test.csv"
+	// }
 ]
