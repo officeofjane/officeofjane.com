b113cf1962c6ad489675892e4f531af37a15efa5
officeofjane <officeofjane@gmail.com>
2026-04-24 13:39:10 +0200

    adapter-cloudflare

--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,10 @@
-import adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: undefined,
-			precompress: false,
-			strict: true
-    }),
-		paths: {
-		  base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
-		}
-	}
+    adapter: adapter()
+  }
 };
 
 export default config;
