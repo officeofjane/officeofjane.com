9a5d7352b693d3e0be24c5941f4576e411f28aba
officeofjane <officeofjane@gmail.com>
2026-05-01 09:22:50 +0100

    site meta

---
+++ src/components/Meta.svelte
@@ -0,0 +1,17 @@
+<script>
+  let {
+    title = "",
+    description = "",
+    url = "https://officeofjane.com"
+  } = $props();
+</script>
+
+<svelte:head>
+  <title>{title}</title>
+  <meta name="description" content={description} />
+  <meta name="author" content="Jane Pong" />
+  
+  <meta property="og:title" content={title} />
+  <meta property="og:url" content={url} />
+  <meta property="og:description" content={description} />
+</svelte:head>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,9 +2,12 @@
   import { setContext } from "svelte";
   import copy from "$data/copy.json";
 
+  import Meta from "$components/Meta.svelte";
   import Index from "$components/Index.svelte";
 
+  const { title, description, url } = copy.meta;
   setContext("copy", copy);
 </script>
 
+<Meta {title} {description} {url} />
 <Index />
